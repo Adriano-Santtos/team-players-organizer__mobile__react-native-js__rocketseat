e066094666333088e16e803c221eefe0edfa993b
Adriano Santos <adrianosantosx2@hotmail.com>
2024-04-24 07:04:39 -0300

    Listando grupos com FlatList

--- src/components/GroupCard/styles.ts
+++ src/components/GroupCard/styles.ts
@@ -1,9 +1,10 @@
 import styled from "styled-components/native";
 import {UsersThree} from "phosphor-react-native"
+import {TouchableOpacity} from "react-native";
 
-export const Container = styled.View`
+export const Container = styled(TouchableOpacity)`
     width: 100%;
-    height: 90%;
+    height: 90px;
 
     background-color: ${({theme}) => theme.COLORS.GRAY_500};
     border-radius: 6px;
@@ -23,6 +24,8 @@ export const Title = styled.Text`
 
 export const Icon = styled(UsersThree).attrs(({theme}) => ({
     size: 32,
-    color: theme.COLORS.GREEN_700
+    color: theme.COLORS.GREEN_700,
+    weight: 'fill',
 }))`
+    margin-right: 20px;
 `;
--- src/screens/Groups/index.tsx
+++ src/screens/Groups/index.tsx
@@ -1,17 +1,32 @@
-import { Header } from '@components/Header';
-import { Highlight } from '@components/HighLight'
-import { Container } from './styles';
+import {Header} from '@components/Header';
+import {Highlight} from '@components/HighLight'
+import {Container} from './styles';
+import {GroupCard} from '@components/GroupCard';
+import {useState} from "react";
+import {FlatList} from "react-native";
 
 export function Groups() {
-  return (
-    <Container>
-      <Header showBackButton/>
+    const [groups, setGroups] = useState(['galera da Rocket','grupo da igreja']);
+    return (
+        <Container>
+            <Header showBackButton/>
 
-      <Highlight 
-        title='Turmas'
-        subTitle='jogue com a sua turma'
-      />
-      
-    </Container>
-  );
+            <Highlight
+                title='Turmas'
+                subTitle='jogue com a sua turma'
+            />
+
+            <FlatList
+                data={groups}
+                keyExtractor={item => item}
+                renderItem={({item}) => (
+                    <GroupCard
+                        title={item}
+                    />
+                )}
+            />
+
+
+        </Container>
+    );
 }
