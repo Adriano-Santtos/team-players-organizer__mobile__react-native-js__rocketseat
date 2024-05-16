c7cc7c2b01716f7d87bc5cf20a103cbe94210f63
Adriano Santos <adrianosantosx2@hotmail.com>
2024-05-16 18:32:57 -0300

    Ajusta componente de ButtonIcon

--- src/screens/Players/index.tsx
+++ src/screens/Players/index.tsx
@@ -1,7 +1,8 @@
-import {Container} from "@screens/Players/styles";
+import {Container, Form} from "@screens/Players/styles";
 import {Header} from "@components/Header";
 import {Highlight} from "@components/HighLight";
 import {ButtonIcon} from "@components/ButtonIcon";
+import {Input} from "@components/Input";
 
 export function Players() {
     return (
@@ -14,7 +15,15 @@ export function Players() {
                 title="Nome da turma"
                 subTitle="adicione a galera e separe os times"
             />
-            <ButtonIcon/>
+            <Form>
+                <Input
+                    placeholder='Nome da pessoa'
+                    autoCorrect={false} //corretor não vai tentar corrigir a entrada
+                />
+                <ButtonIcon
+                    icon='add'
+                />
+            </Form>
         </Container>
 
     )
