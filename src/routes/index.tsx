d1a855b26012e3b06f4236b046c69b15073910db
Adriano Santos <adrianosantosx2@hotmail.com>
2024-05-30 15:23:22 -0300

    Prevenindo o glitch

--- src/routes/index.tsx
+++ src/routes/index.tsx
@@ -1,12 +1,19 @@
 import {NavigationContainer} from '@react-navigation/native';
+import {View} from "react-native";
 
 import {AppRoutes} from "./app.route";
+import React from "react";
+import {useTheme} from "styled-components/native";
 
 
 export function Routes() {
+    const {COLORS} = useTheme()
+
     return (
-        <NavigationContainer>
-            <AppRoutes/>
-        </NavigationContainer>
+        <View style={{flex: 1, backgroundColor: COLORS.GRAY_600}}>
+            <NavigationContainer>
+                <AppRoutes/>
+            </NavigationContainer>
+        </View>
     )
 }
