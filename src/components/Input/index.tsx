3ebb7c2c7a554e4c8c6de20ff01f8631af96a2de
Adriano Santos <adrianosantosx2@hotmail.com>
2024-06-07 13:29:06 -0300

    Melhorando a usabilidade

--- src/components/Input/index.tsx
+++ src/components/Input/index.tsx
@@ -7,11 +7,12 @@ type Props = TextInputProps & {
     inputRef?: React.RefObject<TextInput>;
 }
 
-export function Input({...rest}: Props) {
+export function Input({inputRef, ...rest}: Props) {
     const {COLORS} = useTheme();
 
     return (
         <Container
+            ref={inputRef}
             placeholderTextColor={COLORS.GRAY_300}
             {...rest}
         />
